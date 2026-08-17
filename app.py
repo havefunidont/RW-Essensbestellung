e3d46788c88c1b7ebacfd167fcc2b34988e1a9ec
ihavefevergreat <cosmocaruso39@gmail.com>
2026-08-17 14:09:27 +0200

    Test

--- app.py
+++ app.py
@@ -404,7 +404,7 @@ def administration_delete(resident_id):
     
 if __name__ == '__main__':
     init_db()
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
     # from livereload import Server
     # server = Server(app.wsgi_app)
     # server.watch('.')
