9f31ac7f94895811d5d06c0d21d572d7972f5d85
ihavefevergreat <cosmocaruso39@gmail.com>
2026-08-18 11:48:50 +0200

    Start of modern UI

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request, redirect, flash
 from datetime import datetime, timedelta
 import sqlite3
 
-# RELEASE
+# INDEV
 
 app = Flask(__name__)
-# app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 DB_FILE = "datenbank.db"
 
@@ -206,7 +206,6 @@ def overview():
                     , Orders.* 
                     INNER JOIN Orders
                     ON Orders.residentID = Residents.residentID
-                    WHERE Orders.date = ? 
                     WHERE Orders.date = ? AND Residents.name LIKE ?
                     ORDER BY Residents.room ASC
                     """, (date_filter, f'%{name_filter}%'))
@@ -405,8 +404,8 @@ def administration_delete(resident_id):
     
 if __name__ == '__main__':
     init_db()
-    app.run(host='127.0.0.1', port=5000, debug=False)
-    # from livereload import Server
-    # server = Server(app.wsgi_app)
-    # server.watch('.')
-    # server.serve(port=5000, liveport=35729, host='0.0.0.0')
+    # app.run(host='127.0.0.1', port=5000, debug=False)
+    from livereload import Server
+    server = Server(app.wsgi_app)
+    server.watch('.')
+    server.serve(port=5000, liveport=35729, host='0.0.0.0')
